dad3c71eed99d6ed44092a34c280eb9b5af70955
Zion203 <jesudas.t@kalvium.community>
2024-01-22 10:51:29 +0530

    commit

---
+++ myapp/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Content from './component/content'
+import Context from './component/context'
+
+function App() {
+
+  return (
+    <>
+      <Context>
+        <Content />
+      </Context>
+    </>
+  )
+}
+
+export default App
